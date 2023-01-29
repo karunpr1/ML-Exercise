4c3688898f0b4b530b1d3c85ab3d6855c7fe2afc
arunima-code <arunima@uni-bremen.de>
2023-01-29 17:53:43 +0100

    Branch for the regression task

---
+++ Time_series_data.py
@@ -0,0 +1,24 @@
+# Load the dataset
+
+
+# Investigate the contents of the dataset
+
+
+# Preprocess the dataset
+
+# Choose the learning algorithm
+
+# Define the hyperparameters to tune
+
+# Perform GridSearchCV to find the best hyperparameters
+
+# Get the best hyperparameters
+    
+# Split the data into training and testing sets
+
+# Train the model
+
+# Predict on the test set
+
+# Evaluate the performance
+
